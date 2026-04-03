faf07819c9177d55940209715c47ead38fb0f13c
melinnicri <melinnicri@gmail.com>
2026-04-03 18:57:20 -0300

    Primer commit: análisis de asesinatos

--- wrangle-data.R
+++ wrangle-data.R
@@ -3,3 +3,17 @@ murders <- read_csv("data/murders.csv")
 murders <-murders %>% mutate(region = factor(region),
                              rate = total / population * 10^5)
 save(murders, file = "rdas/murders.rda")
+head(murders)
+summary(murders)
+murders <- murders %>%
+  mutate(rate = total / population * 100000)
+murders %>%
+  arrange(desc(rate)) %>%
+  head(10)
+library(ggplot2)
+
+ggplot(murders, aes(x = population, y = total, color = region)) +
+  geom_point() +
+  scale_x_log10() +
+  scale_y_log10() +
+  labs(title = "Murders vs Population by State")
